b8b4a755c9f2dbd01403dadaa66abd113a525269
spwoodcock <sam.woodcock@protonmail.com>
2024-02-28 18:23:26 +0000

    build: add base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/qrcodes/",
   server: {
     host: "0.0.0.0",
     port: 3000,
